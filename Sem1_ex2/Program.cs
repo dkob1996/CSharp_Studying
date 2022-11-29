1c62b9576fd48f2c90d8d69b1d6e401a81b04ad2
dkob1996 <dkob1996@gmail.com>
2022-11-30 01:12:50 +0600

    create new ex5 sem 1 and fix ex 2-4 sem1

--- Sem1_ex2/Program.cs
+++ Sem1_ex2/Program.cs
@@ -1,7 +1,12 @@
-﻿int numberA = int.Parse(Console.ReadLine());
-int numberB = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да a = 2, b = 10 -> нет a = 9, b = -3 -> да a = -3 b = 9 -> нет
+//20 мин
 
-if ((numberB * numberB) == numberA)
+
+int numberA = int.Parse(Console.ReadLine());     //Ввод первого числа с клавиатуры
+int numberB = int.Parse(Console.ReadLine());     //Ввод второго числа с клавиатуры
+
+if ((numberB * numberB) == numberA)              
 {
     Console.WriteLine("Good");
 }
