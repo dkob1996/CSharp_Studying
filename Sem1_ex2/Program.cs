a72d592a2d7b2e801b20dd96ee6c0fb3969d3f10
dkob1996 <dkob1996@gmail.com>
2022-11-29 23:49:58 +0600

    create exmp2 sem1

---
+++ Sem1_ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if ((numberB * numberB) == numberA)
+{
+    Console.WriteLine("Good");
+}
+else
+{
+    Console.WriteLine("Bad");
+}
