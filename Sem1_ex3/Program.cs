a222b4055b4b3426a2cc2d44942e24cc095c1b50
dkob1996 <dkob1996@gmail.com>
2022-11-30 00:23:00 +0600

    create ex3 sem1

---
+++ Sem1_ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Напишите номер от 1 до 7 включительно");
+int day = int.Parse(Console.ReadLine());
+
+if ((day>0) & (day<8))
+{
+  if (day == 1)
+  {
+    Console.WriteLine("Понедельник");
+  }
+  if (day == 2)
+  {
+    Console.WriteLine("Вторник");
+  }
+  if (day == 3)
+  {
+    Console.WriteLine("Среда");
+  }
+  if (day == 4)
+  {
+    Console.WriteLine("Четверг");
+  }
+  if (day == 5)
+  {
+    Console.WriteLine("Пятница! Ура!");
+  }
+  if (day == 6)
+  {
+    Console.WriteLine("Суббота! Ура!");
+  }
+  if (day == 7)
+  {
+    Console.WriteLine("Воскресенье( Завтра на работу(");
+  }
+}
+else
+{
+    Console.WriteLine("Некорректное число дня недели");
+}
