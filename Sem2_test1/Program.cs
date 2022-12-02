0893eb2891a11b32a3b6b66d316265a03d235b1c
dkob1996 <dkob1996@gmail.com>
2022-12-03 00:54:26 +0600

    create annptions of techer

---
+++ Sem2_test1/Program.cs
@@ -0,0 +1,12 @@
+﻿//ввод с клавиатуры до тех пор пока будут введены числа а не слова
+
+bool isParsed = false;
+int number = 0;
+while (isParsed == false)
+{
+    isParsed = int.TryParse(Console.ReadLine(), out number);
+    if(isParsed == false)
+    {
+        Console.WriteLine("Пиши числа!");
+    }
+}
