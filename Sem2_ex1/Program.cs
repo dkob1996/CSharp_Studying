dda0236d060a9a9d4722df4d832bd740c39d8691
dkob1996 <dkob1996@gmail.com>
2022-12-02 23:19:12 +0600

    Create sem2 1 exercise

---
+++ Sem2_ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+
+int randomNumber = random.Next(10,100);
+
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10 % 10;
+
+if (lastDigit > firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine(firstDigit);
+}
+
+Console.WriteLine(randomNumber);
+Console.WriteLine(lastDigit);
+Console.WriteLine(firstDigit);
