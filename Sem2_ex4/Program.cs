a184a6f3912fb656ff3df4b5e845dbda6bc74245
dkob1996 <dkob1996@gmail.com>
2022-12-03 00:44:14 +0600

    create sem2 ex 4 and ex5, create homework sem2 ex1

---
+++ Sem2_ex4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет,
+//кратно ли оно одновременно 7 и 23.
+//14 -> нет 46 -> нет 161 -> да
+
+//Random random = new Random();                          //
+
+//int randomNumber1 = random.Next(10,100);               //
+
+//Console.WriteLine(randomNumber1);                      //        
+
+int randomNumber1 = int.Parse(Console.ReadLine());
+
+if (randomNumber1 % 7 ==0 && randomNumber1 % 23 ==0)                 //
+{
+    Console.WriteLine("да");                //
+}
+else
+{
+    Console.WriteLine("нет");              //
+}
---
+++ Sem2_ex5/Program.cs
@@ -0,0 +1,14 @@
+﻿//16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+//5, 25 -> да -4, 16 -> да 25, 5 -> да 8,9 -> нет
+
+int randomNumber1 = int.Parse(Console.ReadLine());                //ввод первой переменной с клавиатуры
+int randomNumber2 = int.Parse(Console.ReadLine());                //ввод второй переменной с клавиатуры
+
+if ((randomNumber1 * randomNumber1 == randomNumber2) | (randomNumber2 * randomNumber2 == randomNumber1))
+{
+        Console.WriteLine("да");  
+}
+else
+{
+        Console.WriteLine("нет");
+}
