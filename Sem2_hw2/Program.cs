d4a67197184783dbb558b602262a30fb2ff313cd
dkob1996 <dkob1996@gmail.com>
2022-12-05 23:21:07 +0600

    create sem2 ex2 homewrok

---
+++ Sem2_hw2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 13:Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать string[]
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int randomNumber = int.Parse(Console.ReadLine());
+int result = 0;               
+
+Console.WriteLine(randomNumber);                      
+
+if (randomNumber >=100)
+{
+    while (randomNumber > 999)
+    {
+        randomNumber = randomNumber /10;
+    }
+    result = randomNumber % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
