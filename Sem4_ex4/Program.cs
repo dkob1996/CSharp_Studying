ef3b5e53e1e7adc1aae7196594b32c3dedb4f2f2
dkob1996 <dkob1996@gmail.com>
2022-12-10 00:12:04 +0600

    create sem4 ex 2-4

---
+++ Sem4_ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]        
+
+int[] array = RandomZeroOne(8);
+PrintArray(array);
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (var i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write(array[arr.Length - 1]);
+    Console.Write("]");
+}
+
+int[] RandomZeroOne(int number)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
