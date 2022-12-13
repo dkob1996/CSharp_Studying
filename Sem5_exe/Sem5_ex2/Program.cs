1540625583176550d14d8d713fc2bf2ad56b0fcd
dkob1996 <dkob1996@gmail.com>
2022-12-14 03:19:31 +0600

    create sem5 classwork

---
+++ Sem5_exe/Sem5_ex2/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+Console.WriteLine("Введите количество элементов массива");
+int ElementCount = int.Parse(Console.ReadLine());
+
+int[] array2 = CreateMassive(ElementCount);
+
+
+
+PrintArray(array2);
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (var i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write(array2[arr.Length - 1]);
+    Console.Write("]");
+}
+
+
+int[] CreateMassive(int number)
+{
+    int[] array2 = new int[number];
+    int[] array = new int[number];
+    Console.WriteLine("Введите подряд через Enter элементы массива");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+        array2[i] = array[i] * (-1);
+    }
+    return array2;
+}
+
+/*int[] ExchangePlusToMinus(int[] array);
+{
+    int[] array2 = new int[ElementCount];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = array[i] * (-1);
+    }
+    return array2;
+}*/
---
+++ Sem5_exe/Sem5_ex5/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве. [1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+
+Console.WriteLine("Введите кол-во элементов в массиве");
+int NumbersOfElements = int.Parse(Console.ReadLine());
+
+int[] array = RandomZeroOne(NumbersOfElements);
+PrintArray(array);
+
+
+int[] ResultOfSumMassive(int[] array)
+{
+    if (array.Length % 2 == 0)
+    {
+        int[] sum = new int[array.Length / 2];
+        for (int i = 0; i < array.Length - i; i++)
+        {
+            sum[i] = array[i] + array[array.Length - 1 - i];
+        }
+        return sum;
+    }
+    else
+    {
+        int[] sum = new int[array.Length / 2 + 1];
+        for (int i = 0; i < array.Length - i; i++)
+        {
+            if (i == (array.Length - 1 - i))
+            {
+                sum[i] = array[i];
+            }
+            else
+            {
+                sum[i] = array[i] + array[array.Length - 1 - i];
+            }
+        }
+        return sum;
+    }
+
+}
+
+
+
+int[] SumMassive = ResultOfSumMassive(array);
+
+PrintSum(SumMassive);
+
+void PrintSum(int[] arr)
+{
+    Console.WriteLine("Массив суммы чисел");
+    Console.Write("[");
+    for (var i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write(SumMassive[arr.Length - 1]);
+    Console.Write("]");
+}
+
+
+
+void PrintArray(int[] arr)
+{
+    Console.WriteLine("Массив случайных чисел");
+    Console.Write("[");
+    for (var i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write(array[arr.Length - 1]);
+    Console.WriteLine("]");
+}
+
+int[] RandomZeroOne(int NumbersOfElements)
+{
+    int[] array = new int[NumbersOfElements];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 10);
+    }
+    return array;
+}
+
