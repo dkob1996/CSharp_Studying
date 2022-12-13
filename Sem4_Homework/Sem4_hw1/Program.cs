6109a9f545969dcfb06aa2288f136e4e5601c74d
dkob1996 <dkob1996@gmail.com>
2022-12-13 18:53:26 +0600

    create: wrong result of sem4 hw

--- Sem4_Homework/Sem4_hw1/Program.cs
+++ Sem4_Homework/Sem4_hw1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int number = EnterNumber("Введите число: ");
+
+Console.WriteLine(MultipleNumbers(number));
+
+int MultipleNumbers(int num)
+{
+    int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+int EnterNumber(string text)
+{
+    bool isParsed = false;
+    int number = 0;
+
+    while (!isParsed)
+    {
+        Console.Write(text);
+        isParsed = int.TryParse(Console.ReadLine(), out number);
+    }
+    return number;
+}
