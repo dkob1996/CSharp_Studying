116035f26086a6ae9cb72b1b63eae760f4cbf46b
dkob1996 <119069989+dkob1996@users.noreply.github.com>
2022-12-13 22:46:02 +0600

    Delete Sem3_ex2 directory

--- Sem3_ex2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿//Задача No18. Общее обсуждение
-//Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
-
-using Sem3ex1;
-string coordinate = SystemCoordinate.GetNumberOfQuarter(1);
-Console.WriteLine(coordinate);
